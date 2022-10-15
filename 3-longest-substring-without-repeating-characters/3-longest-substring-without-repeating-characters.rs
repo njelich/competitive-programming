38c76799813469d896e4cf8119fe6f9e4ed7d4db
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-10-15 21:58:23 +0200

    Time: 4 ms (72.31%), Space: 2.2 MB (78.00%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.rs
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.rs
@@ -1,22 +1,33 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
         let mut max = 0;
-        let mut idx = 0;
-        let mut chars = HashSet::new();
+        let mut start = 0;
+        let mut chars = HashMap::new();
+        println!("{}", s);
         
-        for c in s.chars() {
-            while chars.contains(&c) {
-                chars.remove(&s.chars().nth(idx).unwrap());
-                idx += 1;
+        for (end, c) in s.char_indices() {
+            let dupe = chars.entry(c).or_insert(end);
+            if end != *dupe {
+                max = max.max(end - start);
+                if(*dupe >= start) {
+                    start = *dupe + 1;
+                }
+                *dupe = end;
             }
-            
-            chars.insert(c);
-            
-            max = max.max(chars.len());
         }
         
-        max as i32
+        max.max(s.len() - start) as i32
     }
 }
+
+// 0 d, a: 0;
+// 1 w, 
+// 2 d, idx = 2, dupe = 2
+// 3 f
+
+// 0 d, a - 0
+// 1 w, b - 1, a - 0
+// 2 d, idx = 2, dupe = 2
+// 3 w
