b6f695fe155e9eb49fe96c93966806ec9951be00
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-10-16 01:38:11 +0200

    Time: 54 ms (15.00%), Space: 2.4 MB (25.00%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rs
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rs
@@ -4,8 +4,8 @@ impl Solution {
     pub fn find_anagrams(s2: String, s1: String) -> Vec<i32> {
         let s1: Vec<char> = s1.chars().collect();
         let s2: Vec<char> = s2.chars().collect();
-        let mut f_s1: HashMap<char, i32> = HashMap::with_capacity(s2.len());
-        let mut f_s2: HashMap<char, i32> = HashMap::with_capacity(s2.len());
+        let mut f_s1: HashMap<char, i32> = HashMap::with_capacity(s1.len());
+        let mut f_s2: HashMap<char, i32> = HashMap::with_capacity(s1.len());
         let mut l = 0;
         
         let mut res = Vec::new();
