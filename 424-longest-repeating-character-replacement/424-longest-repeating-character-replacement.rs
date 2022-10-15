91b28a18c2425cfa200e85f139e69d1cad7a5a61
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-10-15 22:50:34 +0200

    Time: 8 ms (49.02%), Space: 2.3 MB (19.61%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.rs
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.rs
@@ -14,28 +14,12 @@ impl Solution {
             count = count.max(*chars.entry(s[end]).or_insert(0));
             max = max.max(end - start);
             while end - start + 1 > (k as usize) + count {
-                println!("w: {} {} {}", start, end, count);
                 *chars.entry(s[start]).or_insert(0) -= 1;
                 start += 1;
                 
             }
-            
-            println!("e: {} {} {} {}", start, end, count, s[end]);
         }
         
         max.max(s.len() - start) as i32
     }
 }
-
-
-//   unsafe {
-//         let count = chars.entry(s[end]).or_insert(0);
-//         *count += 1;
-//         let count = count.clone();
-//         while end - start > (k as usize) + count {
-//             println!("while: {} {} {} {}", start, end, (k as usize), count);
-//             *chars.get_mut(s[start]).unwrap() -= 1;
-//             start += 1;
-
-//         }
-//     }
