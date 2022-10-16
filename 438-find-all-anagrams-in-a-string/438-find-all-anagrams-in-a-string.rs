92bcec53f01771851615246e4d32d461bd58f316
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-10-16 11:10:00 +0200

    Time: 16 ms (38.75%), Space: 2.5 MB (7.50%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rs
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.rs
@@ -4,28 +4,36 @@ impl Solution {
     pub fn find_anagrams(s2: String, s1: String) -> Vec<i32> {
         let s1: Vec<char> = s1.chars().collect();
         let s2: Vec<char> = s2.chars().collect();
-        let mut f_s1: HashMap<char, i32> = HashMap::with_capacity(s1.len());
-        let mut f_s2: HashMap<char, i32> = HashMap::with_capacity(s1.len());
+        let mut f: HashMap<char, i32> = HashMap::with_capacity(s1.len());
         let mut l = 0;
         
         let mut res = Vec::new();
         
+        for i in 0..s1.len() {
+            *f.entry(s1[i]).or_default() += 1;
+        }
+        
         for i in 0..s2.len() {
-            *f_s2.entry(s2[i]).or_default() += 1;
-            if(i < s1.len()) {
-                *f_s1.entry(s1[i]).or_default() += 1;
-            } else {
-                let c = f_s2.entry(s2[l]).or_default();
-                if *c == 1 { 
-                    f_s2.remove(&s2[l]);
-                } else {
-                    *c -= 1;
+            if(i >= s1.len()) {
+                if let Some(mut e) = f.get_mut(&s2[l]) {
+                    *e += 1;
                 }
                 l += 1;
             }
+            
+            if let Some(mut e) = f.get_mut(&s2[i]) {
+                *e -= 1;
+            }
 
-            if(i + 1 >= s1.len() && f_s1 == f_s2) {
-                res.push(l as i32);
+            if(i + 1 >= s1.len()) {
+                let mut sum = 0;
+                for v in f.values() {
+                    sum += v.abs();
+                }
+                if (sum == 0) {
+                    res.push(l as i32);
+                }
+                
             }
             
         }
