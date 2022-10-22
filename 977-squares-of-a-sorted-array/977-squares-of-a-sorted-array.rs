bef2fbd29bebe924dbb0d893a3dda7dc7ed90d3a
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-10-22 19:24:27 +0200

    Time: 20 ms (47.58%), Space: 2.2 MB (58.36%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.rs
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.rs
@@ -5,17 +5,19 @@ impl Solution {
         }
         let mut res: Vec<i32> = Vec::with_capacity(nums.len());
         let (mut l, mut r) = (0, nums.len() - 1);
-        while l != r {
-            if (nums[l].abs() > nums[r]) {
-                res.push(nums[l].pow(2));
-                l += 1;
-            } else {
-                res.push(nums[r].pow(2));
-                r -= 1;
+        unsafe {
+            res.set_len(r+1);
+            while l != r {
+                if (nums[l].abs() > nums[r]) {
+                    *res.get_unchecked_mut(r-l) = nums[l].pow(2);
+                    l += 1;
+                } else {
+                    *res.get_unchecked_mut(r-l) = nums[r].pow(2);
+                    r -= 1;
+                }
             }
+            *res.get_unchecked_mut(0) = nums[l].pow(2);
         }
-        res.push(nums[l].pow(2));
-        res.reverse();
         res
     }
 }
