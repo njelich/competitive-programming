6294bb8172fb771ead39200297132a583e9d3f40
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-11-02 16:59:21 +0000

    Time: 50 ms (42.33%), Space: 3.6 MB (36.50%) - LeetHub

--- 15-3sum/15-3sum.rs
+++ 15-3sum/15-3sum.rs
@@ -11,6 +11,9 @@ impl Solution {
         let (mut l, mut r, mut res) = (0, 0, 0);
         
         for i in 0..(nums.len() - 2) {
+            if i > 1 && nums[i] == nums[i - 1] {
+                continue;
+            }
             l = i + 1;
             r = nums.len() - 1;
             res = -nums[i];
