910ef76de929ef1d69ec903de3320f34cff9d00f
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-11-02 16:54:09 +0000

    Time: 364 ms (16.89%), Space: 248 MB (5.44%) - LeetHub

---
+++ 15-3sum/15-3sum.rs
@@ -0,0 +1,34 @@
+impl Solution {
+    pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut found = Vec::with_capacity(nums.len() / 3);
+
+        if nums.len() < 3 {
+            return found;
+        }
+
+        nums.sort();
+
+        let (mut l, mut r, mut res) = (0, 0, 0);
+
+        
+        for i in 0..(nums.len() - 2) {
+            l = i + 1;
+            r = nums.len() - 1;
+            res = -nums[i];
+            while r != l {
+                if nums[r] + nums[l] > res {
+                    r-=1;
+                } else if nums[r] + nums[l] < res {
+                    l+=1;
+                } else {
+                    found.push(vec![nums[i], nums[l], nums[r]]);
+                    l+=1;
+                }
+            }
+        }
+        
+        found.sort();
+        found.dedup();
+        found
+    }
+}
