9a387dd1c80e177fd250e7a8600e79f7bee15c9a
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-11-03 13:08:33 +0000

    Time: 14 ms (98.64%), Space: 2.1 MB (82.03%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.rs
+++ 16-3sum-closest/16-3sum-closest.rs
@@ -4,37 +4,22 @@ impl Solution {
         
         let mut found: i32 = nums[0] + nums[1] + nums[nums.len() - 1];
         
-        let (mut l, mut r, mut res) = (0, 0, 0);
+        let (mut l, mut r, mut sum) = (0, 0, 0);
         
         for i in 0..(nums.len() - 2) {
-             //Some checks that make a massive memory savings in the `found` vector
-            // if i > 1 && nums[i] == nums[i - 1] {
-            //     continue;
-            // }
             l = i + 1;
             r = nums.len() - 1;
-            res = -nums[i] + target;
             while r != l {
-                if((found - target).abs() > (nums[i] + nums[r] + nums[l] - target).abs()) {
-                    found = nums[i] + nums[r] + nums[l];
+                sum = nums[i] + nums[r] + nums[l] - target;
+                if((found - target).abs() > (sum).abs()) {
+                    found = sum + target;
                 }
-                
-                // //Some checks that make a massive memory savings in the `found` vector
-                // if l > i + 1 && nums[l] == nums[l - 1] {
-                //     l += 1;
-                //     continue;
-                // }
-                // if r < nums.len() - 1 && nums[r] == nums[r+1] {
-                //     r -= 1;
-                //     continue;
-                // }
-                
-                if nums[i] + nums[r] + nums[l] > target {
+                if sum > 0 {
                     r-=1;
-                } else if nums[i] + nums[r] + nums[l] < target {
+                } else if sum < 0 {
                     l+=1;
                 } else {
-                    return nums[i] + nums[r] + nums[l];
+                    return sum + target;
                 }
             }
         }
