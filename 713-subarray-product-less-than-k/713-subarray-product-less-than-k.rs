b90e4194b2f9751936676ac17dc060007c14765f
Noah Jelich <12912633+njelich@users.noreply.github.com>
2022-11-03 16:04:01 +0000

    Time: 18 ms (66.67%), Space: 2.4 MB (46.67%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.rs
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.rs
@@ -1,24 +1,22 @@
 impl Solution {
     pub fn num_subarray_product_less_than_k(nums: Vec<i32>, k: i32) -> i32 {
-        let mut found = 0;
-        
-        let mut res;
-        
-        for i in 0..nums.len() {
-            res = nums[i];
-            if(res >= k) {
-                continue;
-            }
-            found+=1;
-            for r in (i+1)..nums.len() {
-                res*=nums[r];
-                if(res >= k) {
-                    break;
-                }
-                found+=1;
-            }
+        if k == 0 {
+            return 0;
         }
         
-        found
+        let mut found = 0;
+        let mut res = 1;
+        let mut l = 0;
+        
+        for r in 0..nums.len() {
+            res*=nums[r];
+            while(res >= k && l <= r) {
+                res /= nums[l];
+                l+=1;
+            }
+            found += r - l + 1;
+        }
+        
+        found as i32
     }
 }
