f83a9cbb18b12fa9459b23b9d3b419beba13f0ae
Noah Jelich <12912633+njelich@users.noreply.github.com>
2023-02-04 21:32:49 +0100

    Time: 83 ms (37.09%), Space: 46.2 MB (57.35%) - LeetHub

--- 704-binary-search/704-binary-search.ts
+++ 704-binary-search/704-binary-search.ts
@@ -1,9 +1,7 @@
 function search(nums: number[], target: number): number {
     let l = 0, r = nums.length - 1;
-    console.log("start", target, l, r)
     while (l <= r) {
         const i = (l + r) / 2 >> 0;
-        console.log(nums[i], i, l, r)
         if (nums[i] == target) {
             return i;
         } else if (nums[i] > target) {
