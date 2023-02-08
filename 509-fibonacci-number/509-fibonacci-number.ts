ddd7d0ec9a2ab0334267a466aeee980ecf94c11f
Noah Jelich <12912633+njelich@users.noreply.github.com>
2023-02-09 00:12:54 +0100

    Time: 60 ms (90.83%), Space: 43.7 MB (6.98%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.ts
@@ -0,0 +1,10 @@
+function fib(n: number): number {
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+    let [prev, curr] = [1, 1];
+    for (let i = 2; i < n; i++) {
+        // Cool ES6 feature
+        [curr, prev] = [prev + curr, curr];
+    }
+    return curr;
+};
